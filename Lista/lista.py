fe8c65ab40e98b656454a189935305c9fc36ed36
CarlosArguelloDev <carlos.arguello.dev@gmail.com>
2026-03-31 02:30:52 -0600

    feat: Agrega las funciones basicas con listas

---
+++ Lista/lista.py
@@ -0,0 +1,19 @@
+numeros = [1,2,3,4,5]
+
+print(numeros[3])
+# Imprimir el ultimo
+print(numeros[-1])
+# Subconjuntos
+print(numeros[0:4])
+
+# Concatenar Listas
+lista1 = [1, 2, 3 ,4]
+lista2 = [5, 6, 7 ,8]
+
+print(lista1+lista2)
+
+# Longitud de lista
+print(len(numeros))
+
+# Revisar si existe el elemento en la lista
+print(1 in lista1)
