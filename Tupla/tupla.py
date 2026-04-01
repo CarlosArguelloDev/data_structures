de19f77e35c1d6c1545651120884ab8343740dec
CarlosArguelloDev <carlos.arguello.dev@gmail.com>
2026-03-31 18:00:00 -0600

    feat: Agrega las funciones basicas con Tuplas

---
+++ Tupla/tupla.py
@@ -0,0 +1,17 @@
+# Tuplas
+"""  
+  Se suelen usar no para modificar valores si no mas bien para dar información de un conjunto
+  de elementos que no se vayan a modificar
+"""
+# Se pueden acceder de manera similar a las listas
+par = (1, 2, 3, 4)
+lista = [1, 2, 3, 4]
+
+print(par)
+print(lista)
+
+lista[0] = 5
+par[0] = 5
+
+print(par[0])
+print(lista[0])
